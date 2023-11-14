1b48fa1bbb5a54cc194023ab752da1752e068ade
StatixHub <150426487+StatixHub@users.noreply.github.com>
2023-11-14 15:10:07 +0100

    Update Minori.py

--- Minori.py
+++ Minori.py
@@ -47,7 +47,7 @@ FamiglieS= FamiglieS.sort_values(by="Percentuale", ascending=True)
 st.set_page_config(layout="wide")
 #set titolo applicazione
 st.title("StatiX")
-st.write("App per la visualizzazione dei dati statistici relativi al Comune di Lecco (minori, adulti e over 65 anni, scuole e asili)")
+st.write("App per la visualizzazione dei dati statistici relativi al Comune di Lecco (minori, adulti e over 65 anni, scuole e asili)-statistiche aggiornate il 14/11/2023")
 #crea il markdown minori
 st.markdown("# Minori")
 #crea la sidebar con i markdown
