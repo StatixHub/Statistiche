b4cb68d71d9708c431d5075039ba812033a7fa7d
StatixHub <150426487+StatixHub@users.noreply.github.com>
2023-11-14 11:57:34 +0100

    Add files via upload

---
+++ pages/Anziani.py
@@ -0,0 +1,169 @@
+"""
+File: StatiX_AnzianiStreamlit.py
+Author: Chiara Tirendi
+Date: 13/11/2023
+Description: Grafici statistiche anziani
+"""
+
+#importa moduli e pacchetti di python
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+from matplotlib import pyplot as plt
+import streamlit as st
+import plotly.express as px
+import squarify
+import seaborn as sb
+import altair as alt
+
+#importa i db dai csv
+RapportoAbAnzS= pd.read_csv("../Destinazione/CSV/RapportoAbAnz.csv")
+AnzianiQuartS= pd.read_csv("../Destinazione/CSV/AnzianiQuart.csv")
+MF65S = pd.read_csv("../Destinazione/CSV/MF65.csv")
+MQuartieri65S = pd.read_csv("../Destinazione/CSV/MQuartieri65.csv")
+FQuartieri65S = pd.read_csv("../Destinazione/CSV/FQuartieri65.csv")
+MFQuartieri65S = pd.read_csv("../Destinazione/CSV/MFQuartieri65.csv")
+RapportoStrAnzS = pd.read_csv("../Destinazione/CSV/RapportoStrAnz.csv")
+StranieriQuart65S = pd.read_csv("../Destinazione/CSV/StranieriQuart65.csv")
+
+#set pagina su tutta l'ampiezza
+st.set_page_config(layout="wide")
+#set titolo applicazione
+st.title("StatiX")
+st.write("App per la visualizzazione dei dati statistici relativi al Comune di Lecco (minori, adulti e over 65 anni, scuole e asili)")
+#crea il markdown minori
+st.markdown("# Adulti")
+#crea la sidebar con i markdown
+st.sidebar.markdown("# Adulti")
+
+#colori
+colorQuart= ["#FF0066", "#FF6699", "#CC3366", "#993333", "#CC3333", "#990000", "#660000", "#993300", "#CC6633", "#CC9933", "#FFCC33", "#999900", "#666600", "#669900", "#339966"]
+colorMF= ["#33CCCC", "#FF0066"]
+colorFasce= ["#D69EC4", "#B772A1", "#C42B91", "#912F70", "#990066"]
+colorFasceSTR= ["#C0E8FB", "#6ECFF6", "#0093D7", "#0072B9", "#214297"]
+
+#contenitore prima riga
+primaRiga = st.container()
+with primaRiga:
+    #configura numero colonne
+    colpr1, colpr2 = st.columns(2, gap="medium")
+    with colpr1:
+        #GRAFICO RAPPORTO TRA ANZIANI E TOTALE ABITANTI
+        st.subheader("Rapporto (%) tra over 65 e totale abitanti")
+        st.write(RapportoAbAnzS)
+        #st.bar_chart(RapportoAbAnzS, x="Tipologia", y="Percentuale", color="#990066")
+        # crea grafico
+        RAAN = alt.Chart(RapportoAbAnzS).mark_bar(color="#990066").encode(alt.X("Tipologia"), alt.Y("Percentuale"))
+        # label
+        textRAAN = RAAN.mark_text(align="center", baseline="bottom").encode(text="Percentuale")
+        # stampa grafico + label
+        st.altair_chart(RAAN + textRAAN, use_container_width=True)
+
+    with colpr2:
+    #GRAFICO DISTRIBUZIONE ANZIANI NEI QUARTIERI
+        st.subheader("Distribuzione (%) degli over 65 nei quartieri")
+        ANQ = alt.Chart(AnzianiQuartS).mark_line(point=alt.OverlayMarkDef(filled=True, fill="#990066"),
+                                                strokeWidth=2, color="#990066"
+                                                ).encode(alt.X("Quartiere"), alt.Y("Percentuale")).interactive()
+
+        text = ANQ.mark_text(
+            align="center",
+            baseline="middle",
+            dy=-7).encode(text="Percentuale")
+        # stampa grafico + label
+        st.altair_chart(ANQ+text, use_container_width=True)
+        with st.expander("Tabella distribuzione (%) over 65 nei quartieri"):
+            st.write(AnzianiQuartS)
+
+secondaRiga = st.container()
+with secondaRiga:
+    colsr1, colsr2, colsr3, colsr4 = st.columns(4, gap="large")
+    with colsr1:
+        # GRAFICO MASCHI E FEMMINE
+        st.subheader("Maschi e Femmine")
+        st.write(MF65S)
+        figMF, axesMF = plt.subplots()
+        axesMF.pie(x=MF65S["Percentuale"], autopct='%1.1f%%', pctdistance=1.24, colors=colorMF,
+                   wedgeprops={"edgecolor": "white", "linewidth": 1})
+        axesMF.axis('equal')  # Rispetto dell'aspect ratio
+        axesMF.legend(ncol=1, labels=MF65S["Sesso"], title="Legenda", bbox_to_anchor=(1, 1), loc="upper left")
+        st.pyplot(figMF, use_container_width=True)
+    with colsr2:
+        # GRAFICO DISTRIBUZIONE FEMMINE NEI QUARTIERI
+        st.subheader("Distribuzione (%) delle femmine nei quartieri")
+        FANQ = alt.Chart(FQuartieri65S).mark_line(point=alt.OverlayMarkDef(filled=True, fill="#FF0066"),
+                                                    strokeWidth=2, color="#FF0066"
+                                                    ).encode(alt.X("Quartiere"),
+                                                             alt.Y("Percentuale femmine")).interactive()
+
+        text = FANQ.mark_text(
+            align="center",
+            baseline="middle",
+            dy=-7).encode(text="Percentuale femmine")
+        # stampa grafico + label
+        st.altair_chart(FANQ+text, use_container_width=True)
+    with colsr3:
+        # GRAFICO DISTRIBUZIONE MASCHI NEI QUARTIERI
+        st.subheader("Distribuzione (%) dei maschi nei quartieri")
+        MANQ = alt.Chart(MQuartieri65S).mark_line(point=alt.OverlayMarkDef(filled=True, fill="#33CCCC"),
+                                                    strokeWidth=2, color="#33CCCC"
+                                                    ).encode(alt.X("Quartiere"),
+                                                             alt.Y("Percentuale maschi")).interactive()
+
+        text = MANQ.mark_text(
+            align="center",
+            baseline="middle",
+            dy=-7).encode(text="Percentuale maschi")
+        # stampa grafico + label
+        st.altair_chart(MANQ+text, use_container_width=True)
+    with colsr4:
+        # GRAFICO RAPPORTO MASCHI E FEMMINE NEI QUARTIERI
+        st.subheader("Rapporto (%) maschi e femmine nei quartieri")
+        MFQAN1 = alt.Chart(MFQuartieri65S).mark_line(point=alt.OverlayMarkDef(filled=True, fill="#33CCCC"),
+                                                    strokeWidth=2, color="#33CCCC"
+                                                    ).encode(alt.X("Quartiere"),
+                                                             alt.Y("Percentuale maschi")).interactive()
+        MFQAN2 = alt.Chart(MFQuartieri65S).mark_line(point=alt.OverlayMarkDef(filled=True, fill="#FF0066"),
+                                                    strokeWidth=2, color="#FF0066"
+                                                    ).encode(alt.X("Quartiere"),
+                                                             alt.Y("Percentuale femmine")).interactive()
+        # stampa grafico + label
+        st.altair_chart(MFQAN1 + MFQAN2, use_container_width=True)
+        # figMFQAN = px.bar(MFQuartieri65S, x="Quartiere", y=["Percentuale maschi", "Percentuale femmine"],
+        #                 color_discrete_sequence=(colorMF), height=350,
+        #                 labels={"value": "Percentuale"}, text_auto=True)
+        # figMFQAN.update_layout(legend=dict(x=0, y=2), legend_orientation="h")
+        # figMFQAN.update_xaxes(tickangle=270)
+        # st.plotly_chart(figMFQAN, use_container_width=True)
+terzaRiga = st.container()
+with terzaRiga:
+    #configura numero colonne
+    coltz1, coltz2 = st.columns(2, gap="medium")
+    with coltz1:
+        #GRAFICO RAPPORTO TRA STRANIERI E TOTALE MINORI
+        st.subheader("Rapporto (%) tra stranieri e totale adulti")
+        st.write(RapportoStrAnzS)
+        # crea grafico
+        RSTRAN = alt.Chart(RapportoStrAnzS).mark_bar(color="#3399FF").encode(alt.X("Tipologia"), alt.Y("Percentuale"))
+        # label
+        textRSTRAN = RSTRAN.mark_text(align="center", baseline="bottom").encode(text="Percentuale")
+        # stampa grafico + label
+        st.altair_chart(RSTRAN + textRSTRAN, use_container_width=True)
+
+    with coltz2:
+        #DISTRIBUZIONE DEGLI STRANIERI NEI QUARTIERI
+        st.subheader("Distribuzione (%) degli stranieri nei quartieri")
+        SANQ = alt.Chart(StranieriQuart65S).mark_line(point=alt.OverlayMarkDef(filled=True, fill="#3399FF"),
+                                                        strokeWidth=2, color="#3399FF"
+                                                        ).encode(alt.X("Quartiere"),
+                                                                 alt.Y("Percentuale")).interactive()
+
+        text = SANQ.mark_text(
+            align="center",
+            baseline="middle",
+            dy=-7).encode(text="Percentuale")
+        # stampa grafico + label
+        st.altair_chart(SANQ+text, use_container_width=True)
+
+        with st.expander("Tabella distribuzione (%) degli stranieri nei quartieri"):
+            st.write(StranieriQuart65S)
---
+++ pages/Stato_civile.py
@@ -0,0 +1,145 @@
+"""
+File: Stato_civileStreamlit.py
+Author: Chiara Tirendi
+Date: 13/11/2023
+Description: Grafici statistiche stato civile
+"""
+
+#importa moduli e pacchetti di python
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+from matplotlib import pyplot as plt
+import streamlit as st
+import plotly.express as px
+import squarify
+import altair as alt
+import seaborn as sb
+import plotly.graph_objects as go
+
+
+RapportoAbSCivS=pd.read_csv("../Destinazione/CSV/RapportoAbSCiv.csv")
+SLEtaS=pd.read_csv("../Destinazione/CSV/SLEta.csv")
+SLEta017GS=pd.read_csv("../Destinazione/CSV/SLEta017G.csv")
+SLEta1864GS=pd.read_csv("../Destinazione/CSV/SLEta1864G.csv")
+SLEta65GS=pd.read_csv("../Destinazione/CSV/SLEta65G.csv")
+QuartieriSLS=pd.read_csv("../Destinazione/CSV/QuartieriSL.csv")
+SoloQuartieriS=pd.read_csv("../Destinazione/CSV/SoloQuartieri.csv")
+
+
+#set pagina su tutta l'ampiezza
+st.set_page_config(layout="wide")
+#set titolo applicazione
+st.title("StatiX")
+st.write("App per la visualizzazione dei dati statistici relativi al Comune di Lecco (minori, adulti e over 65 anni, scuole e asili)")
+#crea il markdown minori
+st.markdown("# Stato civile libero")
+st.write("Per stato civile libero si intende: nubili/celibi, vedovi, divorziati")
+
+#crea la sidebar con i markdown
+st.sidebar.markdown("# Stato civile libero")
+
+colorFasce= ["#6b007b", "#70B0E0","#9B0065"]
+
+
+primaRiga = st.container()
+with primaRiga:
+    #configura numero colonne
+    colpr1, colpr2 = st.columns(2, gap="large")
+    with colpr1:
+        #GRAFICO RAPPORTO TRA ABITANI DI STATO LIBERO E TOTALE ABITANTI
+        st.subheader("Rapporto (%) tra abitanti stato libero e totale abitanti")
+        st.write(RapportoAbSCivS)
+        #crea grafico
+        RAB= alt.Chart(RapportoAbSCivS).mark_bar(color="#990066").encode(alt.X("Tipologia"), alt.Y("Percentuale"))
+        #label
+        textRAB = RAB.mark_text(align="center", baseline="bottom").encode(text="Percentuale")
+        #stampa grafico + label
+        st.altair_chart(RAB+textRAB, use_container_width=True)
+    with colpr2:
+        #GRAFICO RAPPORTO TRA STATO E FASCE DI ETA'
+        st.subheader("Rapporto (%) tra abitanti stato libero e fasce di età")
+        st.write(SLEtaS)
+        figSLE, axesSLE = plt.subplots()
+        axesSLE.pie(x=SLEtaS["Percentuale"], autopct='%1.1f%%', pctdistance=1.24, colors= colorFasce,wedgeprops={"edgecolor": "white", "linewidth": 1})
+        axesSLE.axis('equal')  # Rispetto dell'aspect ratio
+        axesSLE.legend(ncol=1, labels=SLEtaS["Fascia"], title="Legenda", bbox_to_anchor=(1, 1), loc="upper left")
+        st.pyplot(figSLE, use_container_width=True)
+
+secondaRiga = st.container()
+with secondaRiga:
+    #configura numero colonne
+    colsc1, colsc2, colsc3 = st.columns(3, gap="medium")
+    with colsc1:
+        # GRAFICO STATO LIBERO DA 0 A 17 ANNI
+        st.subheader("Stato libero da 0 a 17 anni")
+        figSL017 = px.bar(SLEta017GS, x="Stato civile", y="Percentuale", color="Stato civile", barmode='group', height=400,
+                         text="Percentuale", color_discrete_sequence=("#B6B0FF", "#70B0E0", "#9B0065"))
+        figSL017.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+        figSL017.update_layout(barmode="stack")
+        figSL017.update_xaxes(tickangle=90)
+        st.plotly_chart(figSL017, use_container_width=True)
+        with st.expander("Tabella stato libero da 0 a 17 anni"):
+            st.write(SLEta017GS)
+    with colsc2:
+        # GRAFICO STATO LIBERO DA 18 A 64 ANNI
+        st.subheader("Stato libero da 18 a 64 anni")
+        figSL1864 = px.bar(SLEta1864GS, x="Stato civile", y="Percentuale", color="Stato civile", barmode='group',
+                          height=400,
+                          text="Percentuale", color_discrete_sequence=("#B6B0FF", "#70B0E0", "#9B0065"))
+        figSL1864.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+        figSL1864.update_layout(barmode="stack")
+        figSL1864.update_xaxes(tickangle=90)
+        st.plotly_chart(figSL1864, use_container_width=True)
+        with st.expander("Tabella stato libero da 18 a 64 anni"):
+            st.write(SLEta1864GS)
+    with colsc3:
+        # GRAFICO STATO LIBERO OVER 65 ANNI
+        st.subheader("Stato libero over 65 anni")
+        figSL65 = px.bar(SLEta65GS, x="Stato civile", y="Percentuale", color="Stato civile", barmode='group',
+                          height=400,
+                          text="Percentuale", color_discrete_sequence=("#B6B0FF", "#70B0E0", "#9B0065"))
+        figSL65.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+        figSL65.update_layout(barmode="stack")
+        figSL65.update_xaxes(tickangle=90)
+        st.plotly_chart(figSL65, use_container_width=True)
+        with st.expander("Tabella stato libero over 65 anni"):
+            st.write(SLEta65GS)
+
+terzaRiga = st.container()
+with (terzaRiga):
+    #configura numero colonne
+    coltz1, coltz2 = st.columns(2, gap="medium")
+    with coltz1:
+        # GRAFICO STATO LIBERO DA 0 A 17 ANNI
+        st.subheader("Distribuzione (%) dei minori nei quartieri")
+        figQUA = px.bar(QuartieriSLS, x="Quartiere", y="Percentuale", color="Stato civile", labels={"value": "Percentuale"}, height=450,
+                           text_auto=True, color_discrete_sequence=("#B6B0FF", "#70B0E0", "#9B0065"))
+        figQUA.update_layout(legend=dict(x=0, y=1.2), legend_orientation="h")
+        figQUA.update_xaxes(tickangle=270)
+        st.plotly_chart(figQUA, use_container_width=True)
+
+        with coltz2:
+            # DISTRIBUZIONE FAMIGLIE CON UN SOLO COMPONENTE
+            st.subheader("Distribuzione (%) famiglie con un solo componente nei quartieri")
+
+            FAM1 = alt.Chart(SoloQuartieriS).mark_line(point=alt.OverlayMarkDef(filled=True, fill="#70B0E0"),
+                                                              strokeWidth=2, color="#70B0E0"
+                                                              ).encode(alt.X("Quartiere"),
+                                                                       alt.Y("Percentuale 0-17 anni", title="Percentuale"),
+                                                                       alt.Tooltip(["Percentuale 0-17 anni", "Quartiere"])).interactive()
+
+            FAM2 = alt.Chart(SoloQuartieriS).mark_line(point=alt.OverlayMarkDef(filled=True, fill="#9B0065"),
+                                                       strokeWidth=2, color="#9B0065"
+                                                       ).encode(alt.X("Quartiere"),
+                                                                alt.Y("Percentuale 18-64 anni")).interactive()
+
+            FAM3 = alt.Chart(SoloQuartieriS).mark_line(point=alt.OverlayMarkDef(filled=True, fill="#B6B0FF"),
+                                                       strokeWidth=2, color="#B6B0FF"
+                                                       ).encode(alt.X("Quartiere"),
+                                                                alt.Y("Percentuale sopra 65 anni")).interactive()
+            FAM = FAM1+FAM2+FAM3
+
+            FAM.encode()
+
+            st.altair_chart(FAM, use_container_width=True)
