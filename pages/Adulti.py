6a43a57f31c79d67f9195ba11bdf0a480734d176
StatixHub <150426487+StatixHub@users.noreply.github.com>
2025-08-05 16:04:41 +0200

    Update Adulti.py

--- pages/Adulti.py
+++ pages/Adulti.py
@@ -15,7 +15,6 @@ import plotly.express as px
 import squarify
 import seaborn as sb
 import altair as alt
-alt.renderers.set_embed_options(renderer='svg')
 
 
 #importa i db dai csv
@@ -168,6 +167,7 @@ with terzaRiga:
         st.altair_chart(SADQ + text, use_container_width=True)
         with st.expander("Tabella distribuzione (%) degli stranieri nei quartieri"):
             st.write(StranieriQuart1864S)
+
 
 
 
