0268563576270b34041587eee031fcf306d0f8ea
StatixHub <150426487+StatixHub@users.noreply.github.com>
2025-08-05 15:44:07 +0200

    Update Adulti.py

--- pages/Adulti.py
+++ pages/Adulti.py
@@ -42,24 +42,20 @@ colorMF= ["#FF0066", "#33CCCC"]
 colorFasce= ["#D69EC4", "#B772A1", "#C42B91", "#912F70", "#990066"]
 colorFasceSTR= ["#C0E8FB", "#6ECFF6", "#0093D7", "#0072B9", "#214297"]
 
-#contenitore prima riga
+# contenitore prima riga
 primaRiga = st.container()
 with primaRiga:
-    #configura numero colonne
     colpr1, colpr2 = st.columns(2, gap="medium")
 
     with colpr1:
-        # GRAFICO RAPPORTO TRA ADULTI E TOTALE ABITANTI
         st.subheader("Rapporto (%) tra adulti e totale abitanti")
         st.write(RapportoAbAduS)
 
-        # Grafico a barre
         RAAD = alt.Chart(RapportoAbAduS).mark_bar(color="#990066").encode(
             x=alt.X("Tipologia:N", title="Tipologia"),
             y=alt.Y("Percentuale:Q", title="Percentuale (%)")
         )
 
-        # Etichette sulle barre
         textRAAD = alt.Chart(RapportoAbAduS).mark_text(
             align="center",
             baseline="bottom",
@@ -70,22 +66,18 @@ with primaRiga:
             text=alt.Text("Percentuale:Q")
         )
 
-        # Layer grafico + etichette, con renderer SVG (compatibile Firefox)
-        layered_chart = (RAAD + textRAAD).configure_view(render='svg')
+        layered_chart = RAAD + textRAAD
 
         st.altair_chart(layered_chart, use_container_width=True)
 
     with colpr2:
-        # GRAFICO DISTRIBUZIONE ADULTI NEI QUARTIERI
         st.subheader("Distribuzione (%) degli adulti nei quartieri")
 
-        # Grafico a barre interattivo
         ADQ = alt.Chart(AdultiQuartS).mark_bar(color="#990066").encode(
             x=alt.X("Quartiere:N", title="Quartiere"),
             y=alt.Y("Percentuale:Q", title="Percentuale (%)")
         ).interactive()
 
-        # Etichette sulle barre
         text = alt.Chart(AdultiQuartS).mark_text(
             align="center",
             baseline="middle",
@@ -96,13 +88,13 @@ with primaRiga:
             text=alt.Text("Percentuale:Q")
         )
 
-        # Layer grafico + etichette, con renderer SVG (compatibile Firefox)
-        layered_chart2 = (ADQ + text).configure_view(render='svg')
+        layered_chart2 = ADQ + text
 
         st.altair_chart(layered_chart2, use_container_width=True)
 
         with st.expander("Tabella distribuzione (%) adulti nei quartieri"):
             st.write(AdultiQuartS)
+
 
 
 
@@ -191,6 +183,7 @@ with terzaRiga:
         st.altair_chart(SADQ + text, use_container_width=True)
         with st.expander("Tabella distribuzione (%) degli stranieri nei quartieri"):
             st.write(StranieriQuart1864S)
+
 
 
 
